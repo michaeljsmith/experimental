a5695285911074db87ddbaa3c0a6b56e08f7d6dd
Michael Smith <msmith@msmith.id.au>
2012-09-12 16:16:48 +1000

    Cleanup main.

--- src/main.cpp
+++ src/main.cpp
@@ -78,73 +78,77 @@ inline Class<Widget> mainMenu() {
       button("quit", quit()));
 }
 
-int main(int /*argc*/, char* /*argv*/[])
-{
-	int done;
-	SDL_Event event;
-	SDL_Surface* screen;
-	const SDL_VideoInfo* vi;
-	int width,height;
-	struct sth_stash* stash = 0;
-	float sx,sy,dx,dy,lh;
+int viewportWidth, viewportHeight;
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
-		return -1;
-	}
+inline void initPlatform() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+  {
+    fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
+    exit(1);
+  }
 
-	// Init OpenGL
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-
-	vi = SDL_GetVideoInfo();
-	width = vi->current_w - 20;
-	height = vi->current_h - 80;
-	screen = SDL_SetVideoMode(width, height, 0, SDL_OPENGL);
-	if (!screen)
-	{
-		printf("Could not initialise SDL opengl\n");
-		return -1;
-	}
+  const SDL_VideoInfo* vi = SDL_GetVideoInfo();
+  viewportWidth = vi->current_w - 20;
+  viewportHeight = vi->current_h - 80;
 
 	SDL_WM_SetCaption("FontStash Demo", 0);
+}
 
-//	fnt = sth_create("DroidSansJapanese.ttf", 512,512);
+inline void shutdownPlatform() {
+	SDL_Quit();
+}
+
+inline void initGl() {
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+  SDL_Surface* screen = SDL_SetVideoMode(viewportWidth, viewportHeight, 0, SDL_OPENGL);
+  if (!screen)
+  {
+    printf("Could not initialise SDL opengl\n");
+    exit(1);
+  }
+}
+
+struct sth_stash* stash = 0;
+
+inline void addFont(int idx, string path) {
+  if (!sth_add_font(stash, idx, path.c_str()))
+  {
+    printf("Could not add font.\n");
+    exit(1);
+  }
+}
+
+inline void initializeFonts() {
 	stash = sth_create(512,512);
 	if (!stash)
 	{
 		printf("Could not create stash.\n");
-		return -1;
-	}
-	if (!sth_add_font(stash,0,"data/DroidSerif-Regular.ttf"))
-	{
-		printf("Could not add font.\n");
-		return -1;
-	}
-	if (!sth_add_font(stash,1,"data/DroidSerif-Italic.ttf"))
-	{
-		printf("Could not add font.\n");
-		return -1;
-	}
-	if (!sth_add_font(stash,2,"data/DroidSerif-Bold.ttf"))
-	{
-		printf("Could not add font.\n");
-		return -1;
-	}
-	if (!sth_add_font(stash,3,"data/DroidSansJapanese.ttf"))
-	{
-		printf("Could not add font.\n");
-		return -1;
+    exit(1);
 	}
 
-	done = 0;
+  addFont(0, "data/DroidSerif-Regular.ttf");
+	addFont(1, "data/DroidSerif-Italic.ttf");
+  addFont(2, "data/DroidSerif-Bold.ttf");
+  addFont(3, "data/DroidSansJapanese.ttf");
+}
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+  initPlatform();
+  initGl();
+
+  initializeFonts();
+
+	int done = 0;
 	while (!done)
 	{
+    SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
 			switch (event.type)
@@ -166,7 +170,7 @@ int main(int /*argc*/, char* /*argv*/[])
 		}
 
 		// Update and render
-		glViewport(0, 0, width, height);
+		glViewport(0, 0, viewportWidth, viewportHeight);
 		glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_BLEND);
@@ -174,7 +178,7 @@ int main(int /*argc*/, char* /*argv*/[])
 		glDisable(GL_TEXTURE_2D);
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(0,width,0,height,-1,1);
+		glOrtho(0,viewportWidth,0,viewportHeight,-1,1);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		glDisable(GL_DEPTH_TEST);
@@ -182,6 +186,7 @@ int main(int /*argc*/, char* /*argv*/[])
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
+    float sx,sy,dx,dy,lh;
 		sx = 100; sy = 250;
 
 		sth_begin_draw(stash);
@@ -214,6 +219,7 @@ int main(int /*argc*/, char* /*argv*/[])
 		SDL_GL_SwapBuffers();
 	}
 
-	SDL_Quit();
+  shutdownPlatform();
+
 	return 0;
 }
