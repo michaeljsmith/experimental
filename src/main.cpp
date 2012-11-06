48807c6c932687cd11797944b0d2fb5260c3382d
Michael Smith <msmith@msmith.id.au>
2012-11-06 19:48:57 +1100

    Builds under clang.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,59 @@
+#include <memory>
+#include <functional>
+
+using std::shared_ptr;
+using std::make_shared;
+using std::function;
+
 struct TouchHandler {
-  virtual ~TouchHandler() {}
+  virtual ~TouchHandler();
   virtual void handleTouch(int x, int y) = 0;
 };
+
+TouchHandler::~TouchHandler() {
+}
 
 struct Renderable {
-  virtual ~Renderable() {}
+  virtual ~Renderable();
   virtual void render() = 0;
 };
 
+Renderable::~Renderable() {
+}
+
 struct Widget : public TouchHandler, public Renderable {
+  virtual ~Widget();
   virtual void render() = 0;
   virtual void handleTouch(int x, int y) = 0;
 };
 
-struct _WidgetImpl : public Widget {
-  shared_ptr<Renderable> renderable;
-  shared_ptr<TouchHandler> touchHandler;
+Widget::~Widget() {
+}
 
-  _WidgetImpl(shared_ptr<Renderable> renderable, shared_ptr<TouchHandler> touchHandler):
-    renderable(renderable), touchHandler(touchHandler) {}
+struct WidgetImpl : public Widget {
+  shared_ptr<Renderable> _renderable;
+  shared_ptr<TouchHandler> _touchHandler;
 
-  virtual void render() {
-    renderable->render();
-  }
+  WidgetImpl(shared_ptr<Renderable> renderable, shared_ptr<TouchHandler> touchHandler):
+    _renderable(renderable), _touchHandler(touchHandler) {}
 
-  virtual void handleTouch(int x, int y) {
-    touchHandler->handleTouch(x, y);
-  }
+  virtual void render();
+  virtual void handleTouch(int x, int y);
 };
+
+void WidgetImpl::render() {
+  _renderable->render();
+}
+
+void WidgetImpl::handleTouch(int x, int y) {
+  _touchHandler->handleTouch(x, y);
+}
 
 struct Void {};
 
 // TODO: Specialize Class for void type.
 // TODO: Special version for non-returning expressions?
-void nullRenderable(shared_ptr<Renderable>& self) {
+inline void nullRenderable(shared_ptr<Renderable>& self) {
   struct _NullRenderable : public Renderable {
     virtual void render() {
     }
@@ -42,28 +62,38 @@ void nullRenderable(shared_ptr<Renderable>& self) {
   self = make_shared<_NullRenderable>();
 }
 
-void nullTouchHandler(shared_ptr<TouchHandler>& self, function<void (shared_ptr<TouchHandler>&, Void)>) {
-  struct _NullTouchHandler : public TouchHandler {
-    virtual void handleTouch(int x, int y) {
+inline void nullTouchHandler(shared_ptr<TouchHandler>& self, function<void (shared_ptr<TouchHandler>&, Void)>) {
+  struct NullTouchHandler : public TouchHandler {
+    virtual void handleTouch(int /*x*/, int /*y*/) {
     }
   };
 
-  self = make_shared<_NullRenderable>();
+  self = make_shared<NullTouchHandler>();
 }
 
-void waitForTouch(shared_ptr<TouchHandler>& self, function<void (shared_ptr<TouchHandler>&, Void)> cont) {
+inline void waitForTouch(shared_ptr<TouchHandler>& self, function<void (shared_ptr<TouchHandler>&, Void)> cont) {
   struct TouchHandlerImpl : public TouchHandler {
-    virtual void handleTouch(int x, int y) {
+    shared_ptr<TouchHandler>& _self;
+    function<void (shared_ptr<TouchHandler>&, Void)> _cont;
+
+    TouchHandlerImpl(
+        shared_ptr<TouchHandler>& self2,
+        function<void (shared_ptr<TouchHandler>&, Void)> const& cont2):
+      _self(self2),
+      _cont(cont2) {
+    }
+
+    virtual void handleTouch(int /*x*/, int /*y*/) {
       // ... Test if in area.
 
-      cont(self, Void());
+      _cont(_self, Void());
     }
   };
 
-  self = make_shared<TouchHandlerImpl>();
+  self = make_shared<TouchHandlerImpl>(self, cont);
 }
 
-void quad(shared_ptr<TouchHandler>& self) {
+inline void quad(shared_ptr<Renderable>& self) {
   struct RenderableImpl : public Renderable {
     virtual void render() {
       // ... Render quad
@@ -73,7 +103,7 @@ void quad(shared_ptr<TouchHandler>& self) {
   self = make_shared<RenderableImpl>();
 }
 
-void button(shared_ptr<Widget>& self, function<void (shared_ptr<Widget>&, Void)> cont) {
+inline void button(shared_ptr<Widget>& /*self*/, function<void (shared_ptr<Widget>&, Void)> /*cont*/) {
 }
 
 int main() {
