25ad4f7077034e80ae7f0b733c4c8c6450ec8538
Michael Smith <msmith@msmith.id.au>
2012-11-17 16:44:08 +1100

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <functional>
+#include <memory>
+#include <string>
+#include <iostream>
 
 using std::function;
+using std::shared_ptr;
+using std::make_shared;
+using std::string;
+using std::cout;
 
-template <typename T> using Class = function<void (T&)>;
+template <typename T> using Class = function<shared_ptr<T> ()>;
 
 struct Action {
   function<void ()> perform;
@@ -13,16 +20,35 @@ struct ClickHandler {
 };
 
 inline Class<ClickHandler> onClick(Class<Action> action) {
-  return [=] (ClickHandler& self) {
-    Action _action;
-    action(_action);
+  return [=] () -> shared_ptr<ClickHandler> {
+    auto _action = action();
 
-    self.onClick = [=] (int /*x*/, int /*y*/) {
-      _action.perform();
+    shared_ptr<ClickHandler> self = make_shared<ClickHandler>();
+
+    self->onClick = [=] (int /*x*/, int /*y*/) {
+      _action->perform();
     };
+
+    return self;
+  };
+}
+
+inline Class<Action> print(string text) {
+  return [=] () -> shared_ptr<Action> {
+    auto self = make_shared<Action>();
+    self->perform = [=] () {
+      cout << text;
+    };
+
+    return self;
   };
 }
 
 int main() {
+  auto app = onClick(print("hello\n"));
+  auto _app = app();
+
+  _app->onClick(10, 10);
+
   return 0;
 }
