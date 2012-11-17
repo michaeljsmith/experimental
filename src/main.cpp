16248b3e9c618303ad5d056ea3bcf1a357cef894
Michael Smith <msmith@msmith.id.au>
2012-11-18 08:43:46 +1100

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,23 @@ using Bound = int;
 using AxisBounds = array<Bound, 2>;
 using Bounds = array<AxisBounds, 2>;
 
+using Position = int;
+using Point = array<Position, 2>;
+
+inline bool positionInAxisBounds(Position position, AxisBounds axisBounds) {
+  return position >= axisBounds[0] &&
+    position < axisBounds[1];
+}
+
+inline bool pointInBounds(Point point, Bounds bounds) {
+  for (size_t axis = 0; axis < 2; ++axis) {
+    if (!positionInAxisBounds(point[axis], bounds[axis])) {
+      return false;
+    }
+  }
+  return true;
+}
+
 namespace objects {
   struct Action {
     function<void ()> perform;
@@ -26,24 +43,50 @@ namespace classes {
 }
 
 namespace objects {
+  template <typename T> struct Expression {
+    function<T ()> evaluate;
+  };
+}
+
+namespace classes {
+  template <typename T> using Expression =
+    function<shared_ptr<objects::Expression<T>> ()>;
+
+  template <typename T> Expression<T> literal(T value) {
+    return [=] () -> shared_ptr<objects::Expression<T>> {
+      auto self = make_shared<objects::Expression<T>>();
+      self->evaluate = [=] () {
+        return value;
+      };
+
+      return self;
+    };
+  }
+}
+
+namespace objects {
   struct ClickHandler {
     function<void (int x, int y)> onClick;
   };
 }
 
 namespace classes {
-  using ClickHandler = function<shared_ptr<objects::ClickHandler> ()>;
+  using ClickHandler = function<shared_ptr<objects::ClickHandler> (Expression<Bounds>)>;
 }
 
 namespace classes {
   inline ClickHandler onClick(Action action) {
-    return [=] () -> shared_ptr<objects::ClickHandler> {
+    return [=] (Expression<Bounds> bounds) -> shared_ptr<objects::ClickHandler> {
       auto _action = action();
+      auto _bounds = bounds();
 
       auto self = make_shared<objects::ClickHandler>();
 
-      self->onClick = [=] (int /*x*/, int /*y*/) {
-        _action->perform();
+      self->onClick = [=] (int x, int y) {
+        auto _currentBounds = _bounds->evaluate();
+        if (pointInBounds({{x, y}}, _currentBounds)) {
+          _action->perform();
+        }
       };
 
       return self;
@@ -68,7 +111,7 @@ int main() {
   using namespace classes;
 
   auto app = onClick(print("hello\n"));
-  auto _app = app();
+  auto _app = app(literal<Bounds>({{{{0, 100}}, {{0, 100}}}}));
 
   _app->onClick(10, 10);
 
