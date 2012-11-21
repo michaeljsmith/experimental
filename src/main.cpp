5bf40e5e443e37b6d320a4328dad4b928ff7e782
Michael Smith <msmith@msmith.id.au>
2012-11-21 21:42:00 +1100

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -33,24 +33,37 @@ inline bool pointInBounds(Point point, Bounds bounds) {
 }
 
 struct Widget {
+  Widget() {}
   Widget(function<void (int, int)> _onClick): onClick(_onClick) {}
   function<void (int, int)> onClick;
 };
 
-//inline shared_ptr<Widget> optionsMenu() {
-//}
+inline void app(function<void (Widget)> yield, function<void (int)> finish) {
+  function<void (function<void (Widget)>)> menu2 = [=] (function<void (Widget)> k) {
+    k(Widget([=] (int, int) {
+        finish(3);
+    }));
+  };
 
-inline shared_ptr<Widget> mainMenu() {
-  auto self = make_shared<Widget>([=] (int, int) {
-    cout << "clicked\n";
-    self->onClick
-  });
-  return self;
+  function<void (function<void (Widget)>)> menu1 = [=] (function<void (Widget)> k) {
+    k(Widget([=] (int, int) {
+      menu2(yield);
+    }));
+  };
+
+  menu1(yield);
 }
 
 int main() {
-  auto app = mainMenu();
-  app->onClick(5, 5);
+  Widget _app;
+  app([&_app] (Widget widget) {
+      _app = widget;
+    }, [] (int result) {
+      exit(result);
+    });
+
+  _app.onClick(5, 5);
+  _app.onClick(5, 5);
 
   return 0;
 }
