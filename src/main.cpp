7cc9a4f2a61f038032db22af6dd1ceb045f0c11d
Michael Smith <msmith@msmith.id.au>
2013-03-28 15:26:06 +1100

    Reboot

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,72 @@
-#include <tuple>
+#include <cassert>
 #include <functional>
 
-using std::tuple;
-using std::function;
+template <typename S> using F = std::function<S>;
 
-struct Void {};
+template <typename T> struct Ex {};
 
-template <typename T> struct Value {};
-
-template <typename T, typename R0, typename R1, typename D0, typename D1, typename B0, typename B1> struct LayoutableImpl {
-  tuple<R0, R1> dim;
-  function<tuple<T, tuple<D0, D1>> (tuple<B0, B1>)> gen;
+template <> struct Ex<void> {
+  using Cont = F<void ()>;
+  using Fn = F<void (Cont)>;
+  Fn fn;
+  Ex(Fn const& fn_): fn(fn_) {}
 };
 
-template <typename X> struct DimensionOf {};
-template <typename X> struct BoundsOf {};
+using Void = Ex<void>;
+using Float = Ex<float>;
 
-template <typename T, typename R0, typename R1> using Layoutable = LayoutableImpl<T, R0, R1,
-         typename DimensionOf<R0>::type,
-         typename DimensionOf<R1>::type,
-         typename BoundsOf<R0>::type,
-         typename BoundsOf<R1>::type>;
-
-struct AtLeastLayoutAxis {
-  AtLeastLayoutAxis(float min_): min(min_) {}
-  float min;
-};
-
-template <> struct DimensionOf<AtLeastLayoutAxis> {using type = Void;};
-template <> struct BoundsOf<AtLeastLayoutAxis> {using type = tuple<Value<float>, Value<float>>;};
-
-template <typename T, typename... Tail>
-Layoutable<T, AtLeastLayoutAxis, AtLeastLayoutAxis> vertical(
-    Layoutable<T, AtLeastLayoutAxis, AtLeastLayoutAxis> head,
-    Tail... tail) {
-  float minW = minWidth(head, tail...);
-  float minH = exactHeight(tail...);
-  return Layoutable<T, AtLeastLayoutAxis, AtLeastLayoutAxis>(
-      make_tuple(minW, minH),
-      [=] (tuple<tuple<Value<float>, Value<float>>, tuple<Value<float>, Value<float>>> dim) {
-        return asdf;
-      });
+template <typename T> T evalSync(Ex<T> /*expr*/) {
+  assert(0);
 }
 
+auto waitForever = Void([] (F<void ()> /*k*/) {
+  // Do nothing.
+ });
+
+template <typename T> Ex<T> lit(T const& /*val*/) {
+  assert(0);
+}
+
+struct Rect_ {
+  Float l;
+  Float t;
+  Float w;
+  Float h;
+};
+using Rect = Ex<Rect_>;
+
+inline Rect rect(Float /*l*/, Float /*t*/, Float /*w*/, Float /*h*/) {
+  assert(0);
+}
+
+inline Void display(Rect rect, F<Rect (Void)> genUi) {
+  return callCc([=] (Void exit) {
+    let(genUi(exit), [=] (Rect ui) {
+      return bind(ui, rect,
+        waitForever);
+    });
+  });
+
+  //return [=] (F<void ()> k) {
+  //  auto exit = Void([=] () {
+  //    cleanup();
+  //    k();
+  //  });
+
+  //  auto ui = genUi(exit);
+  //  auto ui_ = 
+  //};
+}
+
+inline Rect mainUi(Void /*exit*/) {
+  assert(0);
+}
+
+auto app = display(rect(lit(0.0f), lit(0.0f), lit(500.0f), lit(500.0f)), mainUi(exit));
+
 int main() {
+
+  evalSync(app);
+
   return 0;
 }
