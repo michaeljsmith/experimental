f0b5394b23e38ce7d5a758737f3abc04ede35fbb
Michael Smith <msmith@msmith.id.au>
2013-03-28 17:50:17 +1100

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 
 template <typename S> using F = std::function<S>;
 
-template <typename T> struct Ex {};
+template <typename T> struct ContOf {typedef void Type(T);};
+template <> struct ContOf<void> {typedef void Type();};
 
-template <> struct Ex<void> {
-  using Cont = F<void ()>;
+template <typename T> struct Ex {
+  using Type = T;
+  using Cont = typename ContOf<T>::Type;
   using Fn = F<void (Cont)>;
   Fn fn;
-  Ex(Fn const& fn_): fn(fn_) {}
+  Ex(Fn fn_): fn(fn_) {}
 };
 
 using Void = Ex<void>;
@@ -27,42 +29,40 @@ template <typename T> Ex<T> lit(T const& /*val*/) {
   assert(0);
 }
 
+template <typename E> struct Val {
+  using T = typename E::Type;
+  Ex<T> get;
+};
+
+template <typename T> Void track(Var<T>, Val<T>) {
+}
+
 struct Rect_ {
-  Float l;
-  Float t;
-  Float w;
-  Float h;
+  Val<Float> l;
+  Val<Float> t;
+  Val<Float> w;
+  Val<Float> h;
 };
 using Rect = Ex<Rect_>;
 
-inline Rect rect(Float /*l*/, Float /*t*/, Float /*w*/, Float /*h*/) {
+inline Rect fixedRect(Float /*l*/, Float /*t*/, Float /*w*/, Float /*h*/) {
   assert(0);
 }
 
 inline Void display(Rect rect, F<Rect (Void)> genUi) {
   return callCc([=] (Void exit) {
-    let(genUi(exit), [=] (Rect ui) {
-      return bind(ui, rect,
-        waitForever);
+    return let(genUi(exit), [=] (Rect ui) {
+      return track(ui, rect,
+                  waitForever);
     });
   });
-
-  //return [=] (F<void ()> k) {
-  //  auto exit = Void([=] () {
-  //    cleanup();
-  //    k();
-  //  });
-
-  //  auto ui = genUi(exit);
-  //  auto ui_ = 
-  //};
 }
 
 inline Rect mainUi(Void /*exit*/) {
   assert(0);
 }
 
-auto app = display(rect(lit(0.0f), lit(0.0f), lit(500.0f), lit(500.0f)), mainUi(exit));
+auto app = display(fixedRect(lit(0.0f), lit(0.0f), lit(500.0f), lit(500.0f)), mainUi(exit));
 
 int main() {
 
